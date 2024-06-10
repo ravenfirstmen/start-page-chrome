9515101daaedb984f0dce4af126adf033e188a72
aacs <antonio.costa.salgado@gmail.com>
2024-06-10 22:21:26 +0100

    Improve fuse search

--- src/services/search.ts
+++ src/services/search.ts
@@ -1,9 +1,9 @@
+import Fuse from 'fuse.js'
 import { Bookmarks } from './bookmarks'
-import { bookmarksSearch } from './fuse-search'
 
 class Search {
   private bookmarks: chrome.bookmarks.BookmarkTreeNode[] = []
-  private flattenedBookmarks: chrome.bookmarks.BookmarkTreeNode[] = []
+  private fuse: Fuse<chrome.bookmarks.BookmarkTreeNode> | null = null
 
   async search(
     term: string | null
@@ -12,16 +12,36 @@ class Search {
       return await this.cachedBookmarks()
     }
 
-    return bookmarksSearch(term.trim(), this.flattenedBookmarks)
+    return this.fuseSearch(term.trim())
   }
 
   // helper
   async cachedBookmarks(): Promise<chrome.bookmarks.BookmarkTreeNode[]> {
     if (!this.bookmarks || !this.bookmarks.length) {
-      ;[this.bookmarks, this.flattenedBookmarks] =
-        await new Bookmarks().getFromStartPage()
+      let flattenedBookmarks: chrome.bookmarks.BookmarkTreeNode[] = []
+      ;[this.bookmarks, flattenedBookmarks] = await new Bookmarks().getFromStartPage()
+      this.prepareFuseSearch(flattenedBookmarks)
     }
     return this.bookmarks
+  }
+
+
+  fuseSearch(term: string): chrome.bookmarks.BookmarkTreeNode[] {
+    const result = this.fuse!.search(term).map((i) => i.item)
+  
+    return [{ id: '', title: 'Bookmarks...', children: result }]
+  }
+  
+  prepareFuseSearch(items: chrome.bookmarks.BookmarkTreeNode[]) {
+    const options = {
+      includeScore: false,
+      isCaseSensitive: false,
+      useExtendedSearch: true,
+      shouldSort: true,
+      keys: ['title'],
+    }
+
+    this.fuse = new Fuse(items, options)
   }
 }
 
